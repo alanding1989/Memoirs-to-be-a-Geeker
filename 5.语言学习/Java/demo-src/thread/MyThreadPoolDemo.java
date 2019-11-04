20afa34b5c7dc6f0eb40005343e3d2b53b53060f
alanding <dw395577197@163.com>
2019-11-04 16:17:18 +0800

    update

--- 5.语言学习/Java/demo-src/thread/MyThreadPoolDemo.java
+++ 5.语言学习/Java/demo-src/thread/MyThreadPoolDemo.java
@@ -1,95 +1,53 @@
-package thread;
+/*
+*第4种获得/使用java多线程的方式，通过线程池
+* （其他三种是：继承Thread类；实现Runnable接口，但是Runnable没有返回值，不抛异常；
+* 实现Callable接口，有返回值，会跑出异常）
+* */
 
 import java.util.concurrent.*;
 
-/**
- * 第四种使用Java多线程的方式，线程池
- */
+//System.out.println(Runtime.getRuntime().availableProcessors());
+//Array Arrays  辅助工具类
+//Collection Collections
+//Executor Executors
 public class MyThreadPoolDemo {
-    public static void main(String[] args) {
-        System.out.println("Fixed Thread Pool");
-        fixedThreadPool();
-        System.out.println("Single Thread Pool");
-        singleThreadPool();
-        System.out.println("Cached Thread Pool");
-        cachedThreadPool();
-        System.out.println("Custom Thread Pool");
-        customThreadPool();
-    }
+    public static void main(String[] args){
+        ExecutorService threadPool = new ThreadPoolExecutor(2,
+                5,
+                1L,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(3),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.DiscardPolicy());
 
-    private static void customThreadPool() {
-        ExecutorService threadPool=
-                new ThreadPoolExecutor(2,
-                        5,
-                        1L,
-                        TimeUnit.SECONDS,
-                        new LinkedBlockingQueue<>(3),
-                        Executors.defaultThreadFactory(),
-                        new ThreadPoolExecutor.AbortPolicy()
-                );
-        try {
-            for (int i = 0; i < 9; i++) {
-                threadPool.execute(() -> {
-                    System.out.println(Thread.currentThread().getName()+"\t办理业务");
+        try{
+            for(int i=1;i<=11;i++){
+                threadPool.execute(()->{
+                    System.out.println(Thread.currentThread().getName()+"\t 办理业务");
                 });
             }
-
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
-        } finally {
+        }finally {
             threadPool.shutdown();
         }
     }
 
-    private static void cachedThreadPool() {
-        //不定量线程
-        ExecutorService threadPool = Executors.newCachedThreadPool();
-        try {
-            for (int i = 0; i < 9; i++) {
-                threadPool.execute(() -> {
-                    System.out.println(Thread.currentThread().getName()+"\t办理业务");
+    private static void threadPoolInit() {
+        //ExecutorService threadPool = Executors.newFixedThreadPool(5);//一池5个处理线程
+        //ExecutorService threadPool = Executors.newFixedThreadPool(1);//一池1个线程
+        ExecutorService threadPool = Executors.newCachedThreadPool();//一池N个线程
+
+        //模拟10个用户来办理业务，每个用户就是一个来自外部的请求线程
+        try{
+            for(int i=1;i<=10;i++){
+                threadPool.execute(()->{
+                    System.out.println(Thread.currentThread().getName()+"\t 办理业务");
                 });
             }
-
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
-        } finally {
-            threadPool.shutdown();
-        }
-    }
-
-    private static void singleThreadPool() {
-        //一池1个线程
-        ExecutorService threadPool = Executors.newSingleThreadExecutor();
-        try {
-            for (int i = 0; i < 9; i++) {
-                threadPool.execute(() -> {
-                    System.out.println(Thread.currentThread().getName() + "\t办理业务");
-                });
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            threadPool.shutdown();
-        }
-    }
-
-    private static void fixedThreadPool() {
-        //一池5个线程
-        ExecutorService threadPool = Executors.newFixedThreadPool(5);
-        //一般常用try-catch-finally
-        //模拟10个用户来办理业务，每个用户就是一个线程
-        try {
-            for (int i = 0; i < 9; i++) {
-                threadPool.execute(() -> {
-                    System.out.println(Thread.currentThread().getName() + "\t办理业务");
-                });
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
+        }finally {
             threadPool.shutdown();
         }
     }
